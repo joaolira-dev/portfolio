a3a6ee980d2324783cda776501ed05a22e526921
joaolira-dev <joaovictorlira06@hotmail.com>
2025-08-05 18:57:07 -0300

    feat: adding stars in component StarBackground

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
-import { ThemeToggle } from "../components/ThemeToggle"
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
 
 export const Home = () => {
-  return(
+  return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Theme Toggle*/}
-      <ThemeToggle/>  
+      <ThemeToggle />
+      <StarBackground />
     </div>
-  )
-}
+  );
+};
