63b7b33151c78c79bd4a66fd107948a4f2f05774
joaolira-dev <joaovictorlira06@hotmail.com>
2025-08-06 08:51:57 -0300

    feat: adding icon to language toggle without logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,17 @@ import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
+import { LanguageToggle } from "../components/LanguageToggle";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Theme Toggle*/}
       <ThemeToggle />
+
+      {/*  Language Toggle*/}
+      <LanguageToggle/>
+      
       {/* Background effects*/}
       <StarBackground />
 
