f604a1c3a6425e0c5ffe4cdb86149627e46ec3f2
joaolira-dev <joaovictorlira06@hotmail.com>
2025-08-08 11:18:41 -0300

    feat: footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import { ArrowUp } from "lucide-react"
+
+export const Footer = () => {
+  return (
+    <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
+      <p className="text-sm"> &copy; {new Date().getFullYear} joaolira-dev. Todos os direitos reservados</p>
+
+      <a href="#hero" className="p-2 rounded-full bg-primary/10 hover:bg-primary/30 text-primary transition-colors">
+        <ArrowUp/>
+      </a>
+    </footer>
+  )
+}
